2b5936a9a46059ab411d1d416f4b2db2796f044c
eunzle95 <eunzle95@vt.edu>
2020-04-13 14:03:55 +0900

    editing search and connecting it to database

--- servers/routes/index.js
+++ servers/routes/index.js
@@ -29,6 +29,7 @@ router.get("/getApts", function (req, res) {
   });
 });
 
+
 router.post("/search", function (req, res) {
   var sort = req.body.sort;
   var maxPrice = req.body.maxPrice;
@@ -37,26 +38,38 @@ router.post("/search", function (req, res) {
 
   console.log(maxPrice);
   console.log(numBed);
-  fs.readFile("servers/apt.json", (err, data) => {
-    if (err) throw err;
-    let apt = JSON.parse(data);
-    //console.log(apt);
-    for (var i = 0; i < apt.length; i++) {
-      var apartment = apt[i];
-
-      if ((maxPrice >= apartment.price) & (numBed == apartment.beds)) {
-        //res.send(apartment);
-        res.json({
-          success: true,
-        });
-      } else {
-        res.json({
-          success: false,
-        });
-      }
-      //console.log(apartment.name);
+  db.query(`SELECT name from roommate_finder.apartments where average_rent <= ${maxPrice}`, (err, rows) => {
+    if (!err) {
+      res.json({
+        success: true,
+      });
+    } else {
+      console.log(`query error: ${err}`);
+      res.json({
+        success: false,
+      });
     }
-  });
+  })
+  // fs.readFile("servers/apt.json", (err, data) => {
+  //   if (err) throw err;
+  //   let apt = JSON.parse(data);
+  //   //console.log(apt);
+  //   for (var i = 0; i < apt.length; i++) {
+  //     var apartment = apt[i];
+
+  //     if ((maxPrice >= apartment.price) & (numBed == apartment.beds)) {
+  //       //res.send(apartment);
+  //       res.json({
+  //         success: true,
+  //       });
+  //     } else {
+  //       res.json({
+  //         success: false,
+  //       });
+  //     }
+  //     //console.log(apartment.name);
+  //   }
+  // });
 });
 
 router.post("/signup", function (req, res) {
