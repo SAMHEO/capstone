9603cb0d0d0d4287e82d3c0ca7484ac4d9ecd00b
Samnyeong Heo <hsn1017@gmail.com>
2020-04-14 21:33:57 +0900

    remove dbconnection from remote

--- servers/dbconnection.js
+++
@@ -1,9 +0,0 @@
-const mysql = require("mysql");
-const connection = mysql.createPool({
-  host: "database-1.crymp8h5hlhr.us-east-2.rds.amazonaws.com",
-  user: "admin",
-  password: "12345678",
-  database: "roommate_finder",
-});
-
-module.exports = connection;
